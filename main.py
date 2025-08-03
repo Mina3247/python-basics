10910bed535f15a81df968faf0d44535ebaf3f84
Mina3247 <minagerges853@yahoo.com>
2025-08-03 18:11:33 +0400

    getting started with programming

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 # setting of dev tools and intro to python
 print("hello world")
+
+# getting started with programming
+a=4
+b=4
+print(a+b)
+
+c="4"
+d="4"
+print(c+d)
+
+print(type(a))
+print(type(c))
